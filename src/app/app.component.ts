81cc5c49115823ecf18defde49c0ec33385a4ddf
Basmala Mohamed Zain El Abidine <102691187+BasmalaMohamed46@users.noreply.github.com>
2024-04-16 23:45:29 +0200

    checkout page design and validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CheckoutComponent } from './Components/checkout/checkout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CheckoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
