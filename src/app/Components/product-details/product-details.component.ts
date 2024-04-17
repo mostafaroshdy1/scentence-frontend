3dfe8e435a32b94cb8d85e5318d10d60f66b0a4a
Engy Ahmed Abdelaziz <engya306@gmail.com>
2024-04-17 20:48:56 +0200

    Add new components and services for product details, reviews, and shipping policy

--- src/app/Components/product-details/product-details.component.ts
+++ src/app/Components/product-details/product-details.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
 import { ApiProductsService } from '../Services/api-products.service';
 import { HttpClientModule } from '@angular/common/http';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { BannerComponent } from '../banner/banner.component';
+import { CommonModule } from '@angular/common';
+import { DescriptionForProductComponent } from '../about-product-details/description-for-product/description-for-product.component';
+import { ReviewsComponent } from '../about-product-details/reviews/reviews.component';
+import { ShippingPolicyComponent } from '../about-product-details/shipping-policy/shipping-policy.component';
+
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
   imports: [
     HttpClientModule,
-    BannerComponent
+    BannerComponent,
+    CommonModule,
+    RouterModule,
+    DescriptionForProductComponent,
+    ReviewsComponent,
+    ShippingPolicyComponent
   ],
   providers:[
     ApiProductsService
@@ -20,13 +30,29 @@ import { BannerComponent } from '../banner/banner.component';
 export class ProductDetailsComponent {
   constructor(private apiService: ApiProductsService , private router:Router) {}
   productDetails: any;
+  mainImage='';
+  countValue=0;
   ngOnInit(): void {
     this.apiService
-      .getProductById(this.router.url.split('/')[2])
-      .subscribe((data: any) => {
-        this.productDetails = data;
-      });
+    .getProductById(this.router.url.split('/')[2])
+    .subscribe((data: any) => {
+      this.productDetails = data;
+      this.mainImage=this.productDetails.image[3];
+    });
   }
 
+  filterImages(images: string[]): string[] {
+    return images.filter((image, index) => index !== 4);
+  }
+
+  decrement(){
+    if(this.countValue>0){
+      this.countValue--;
+    }
+  }
+
+  increment(){
+    this.countValue++;
+  }
 
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,8 @@
 import { Routes } from '@angular/router';
 import { ProductDetailsComponent } from './Components/product-details/product-details.component';
+import { DescriptionForProductComponent } from './Components/about-product-details/description-for-product/description-for-product.component';
+import { ReviewsComponent } from './Components/about-product-details/reviews/reviews.component';
+import { ShippingPolicyComponent } from './Components/about-product-details/shipping-policy/shipping-policy.component';
 
 export const routes: Routes = [
   {path:'products',children:[
