743dfb98f93920c4338963b82390f4ab403b5f29
Belal Abo Ata <belalbelalmohamed1234@gmail.com>
2024-04-23 05:21:00 +0200

    feat: add header component

--- src/app/home-components/all-products/all-products.component.ts
+++ src/app/home-components/all-products/all-products.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
+import { HeaderParagraphComponent } from '../header-paragraph/header-paragraph.component';
 
 @Component({
 	selector: 'app-all-products',
 	standalone: true,
-	imports: [],
+	imports: [HeaderParagraphComponent],
 	templateUrl: './all-products.component.html',
 	styleUrl: './all-products.component.css',
 })
 export class AllProductsComponent {
+	text: { header: string; paragraph: string } = {
+		header: 'All Products',
+		paragraph:
+			'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical',
+	};
+
 	changeProduct(e: Event, type: string) {
 		const productBtns: NodeListOf<HTMLElement> | null =
 			document.querySelectorAll('.filter-btns > button');
