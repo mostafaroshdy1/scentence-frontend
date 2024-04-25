debbccf3378b48e8d43a45cd5df0a1c58c5625b8
Engy Ahmed Abdelaziz <engya306@gmail.com>
2024-04-25 02:05:11 +0200

    feat:Implemented authentication guards for admin routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,19 @@ import { ShopComponent } from './Components/shop/shop.component';
 import { ProductDetailsComponent } from './Components/product-details/product-details.component';
 import { AdminDashboardComponent } from './Components/Admin/admin-dashboard/admin-dashboard.component';
 import { AddProductComponent } from './Components/Admin/add-product/add-product.component';
+import { AdminManageUsersComponent } from './Components/Admin/admin-manage-users/admin-manage-users.component';
+import { DisplayUserDetailsComponent } from './Components/Admin/display-user-details/display-user-details.component';
+import {
+  AdminAuthGuard,
+  AuthGuardService,
+} from './Services/auth-guard.service';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
   {
     path: 'products',
     children: [
@@ -15,6 +26,7 @@ export const routes: Routes = [
   },
   {
     path: 'admin',
+    canActivate: [AuthGuardService],
     children: [
       {
         path: '',
@@ -23,16 +35,25 @@ export const routes: Routes = [
       {
         path: 'products',
         children: [
-          {
-            path: '',
-            component: ShopComponent,
-          },
-          {
-            path: 'add',
-            component: AddProductComponent,
-          },
+          { path: '', component: ShopComponent },
+          { path: 'add', component: AddProductComponent },
           { path: 'edit/:id', component: AddProductComponent },
           { path: ':id', component: ProductDetailsComponent },
+        ],
+      },
+      {
+        path: 'users',
+        children: [
+          {
+            path: '',
+            canActivate: [AuthGuardService],
+            component: AdminManageUsersComponent,
+          },
+          {
+            path: ':id',
+            canActivate: [AuthGuardService],
+            component: DisplayUserDetailsComponent,
+          },
         ],
       },
     ],
