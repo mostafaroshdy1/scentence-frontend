632a34243bbd345b708f520b94c9fd8ca0e5acc9
Engy Ahmed Abdelaziz <engya306@gmail.com>
2024-04-25 02:13:04 +0200

    Implemented authentication and role-based redirection in login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,75 +1,105 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
 import { HttpClientModule } from '@angular/common/http';
 
 @Component({
-	selector: 'app-login',
-	standalone: true,
-	imports: [ReactiveFormsModule,HttpClientModule],
-	providers:[AuthService],
-	templateUrl: './login.component.html',
-	styleUrl: './login.component.css',
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule, HttpClientModule],
+  providers: [AuthService],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
 })
+export class LoginComponent implements OnInit {
+  constructor(private authService: AuthService, private router: Router) {}
+  checkForm = new FormGroup(
+    {
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl('', [
+        Validators.minLength(6),
+        Validators.required,
+      ]),
+    },
+    { updateOn: 'change' }
+  );
 
-export class LoginComponent {
-	constructor(private authService: AuthService, private router: Router) {}
-	checkForm = new FormGroup(
-		{
-			email: new FormControl('', [Validators.email, Validators.required]),
-			password: new FormControl('', [Validators.minLength(6), Validators.required]),
-		},
-		{ updateOn: 'change' },
-	);
+  ngOnInit(): void {
+    const token = localStorage.getItem('token');
+    if (token) {
+      const decodedToken = this.decodeToken(token);
+      localStorage.setItem('role', decodedToken.role);
+      if (decodedToken.role === 'admin') {
+        this.router.navigate(['/admin']);
+      } else {
+        this.router.navigate(['/products']);
+      }
+    }
+  }
+  private decodeToken(token: string): any {
+    const decodedToken = JSON.parse(atob(token.split('.')[1]));
+    return decodedToken;
+  }
 
-	checkData(e: Event) {
-		e.preventDefault();
-		
-		// console.log(
-		// 	this.checkForm.controls.email.dirty,
-		// 	this.checkForm.controls.email.touched,
-		// 	this.checkForm.controls.password,
-		// );
+  checkData(e: Event) {
+    e.preventDefault();
 
-		// if (this.emailValid && this.passwordValid) {
-		// 	console.log('Valid data');
-		// 	// this.router.navigate(['/']);
-		// } else {
-		// 	console.log('Invalid data');
-		// }
+    // console.log(
+    // 	this.checkForm.controls.email.dirty,
+    // 	this.checkForm.controls.email.touched,
+    // 	this.checkForm.controls.password,
+    // );
 
-		if(this.checkForm.valid){
-			const  {email,password} = this.checkForm.value;
-			this.authService.login(email, password).subscribe(
-				(response) => {
-				  console.log('Login successful:', response);
-				  this.authService.setToken(response.token);
-				//   this.router.navigate(['/dashboard']);
-				},
-				(error) => {
-				  console.error('Login failed:', error);
-				}
-			  );
-		} else {
-			console.log('Form is invalid');
-		}
-	}
+    // if (this.emailValid && this.passwordValid) {
+    // 	console.log('Valid data');
+    // 	// this.router.navigate(['/']);
+    // } else {
+    // 	console.log('Invalid data');
+    // }
 
-	get emailChanged(): boolean {
-		const email = this.checkForm.controls.email;
-		return email.dirty && email.touched;
-	  }
-	
-	  get passwordChanged(): boolean {
-		const password = this.checkForm.controls.password;
-		return password.dirty && password.touched;
-	  }
-	
-	  get emailValid(): boolean {
-		return this.checkForm.controls.email.valid;
-	  }
-	  get passwordValid(): boolean {
-		return this.checkForm.controls.password.valid;
-	  }
+    if (this.checkForm.valid) {
+      const { email, password } = this.checkForm.value;
+      this.authService.login(email, password).subscribe({
+        next: (response) => {
+          console.log('Login successful:', response);
+          localStorage.setItem('token', response.token);
+          const decodedToken = this.decodeToken(response.token);
+          localStorage.setItem('role', decodedToken.role);
+          if (decodedToken.role === 'admin') {
+            this.router.navigate(['/admin']);
+          } else {
+            this.router.navigate(['/products']);
+          }
+        },
+        error: (error) => {
+          console.error('Login failed:', error);
+        },
+      });
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+
+  get emailChanged(): boolean {
+    const email = this.checkForm.controls.email;
+    return email.dirty && email.touched;
+  }
+
+  get passwordChanged(): boolean {
+    const password = this.checkForm.controls.password;
+    return password.dirty && password.touched;
+  }
+
+  get emailValid(): boolean {
+    return this.checkForm.controls.email.valid;
+  }
+  get passwordValid(): boolean {
+    return this.checkForm.controls.password.valid;
+  }
 }
