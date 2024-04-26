a424ab5fa42a127456ea86d1c227777e2b11a712
Mostafa Elgreatly <66712535+mostafaroshdy1@users.noreply.github.com>
2024-04-26 12:44:00 +0300

    fix: import path of cart service

--- src/app/Components/orders-history/orders-history.component.ts
+++ src/app/Components/orders-history/orders-history.component.ts
@@ -3,7 +3,7 @@ import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { OrdersService } from '../../Services/orders.service';
-import { CartService } from '../../cart.service';
+import { CartService } from '../../Services/cart.service';
 
 @Component({
   selector: 'app-orders-history',
