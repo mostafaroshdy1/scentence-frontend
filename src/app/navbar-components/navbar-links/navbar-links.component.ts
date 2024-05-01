758f6a7e1508ec90617afcca063e28be4520dfc6
Basmala Mohamed Zain El Abidine <102691187+BasmalaMohamed46@users.noreply.github.com>
2024-05-02 02:40:59 +0300

    refactor:logo and navbar edited

--- src/app/navbar-components/navbar-links/navbar-links.component.ts
+++ src/app/navbar-components/navbar-links/navbar-links.component.ts
@@ -1,23 +1,27 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { faCircleXmark, faBars } from '@fortawesome/free-solid-svg-icons';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-navbar-links',
   standalone: true,
-  imports: [RouterModule, FontAwesomeModule],
+  imports: [RouterModule, FontAwesomeModule,CommonModule],
   templateUrl: './navbar-links.component.html',
   styleUrl: './navbar-links.component.css',
 })
 export class NavbarLinksComponent {
+  constructor(private route: ActivatedRoute,private router:Router) {}
+  clicked = false;
   links: any[] = [
     { name: 'home', link: '/' },
-    { name: 'shop', link: '/shop' },
-    { name: 'product', link: '/products' },
+    { name: 'profile', link: '/profile/info'},
+    { name: 'products', link: '/products' },
     { name: 'Wish list', link: '/wishlist' },
     { name: 'about us', link: '/aboutus' },
-    { name: 'contact', link: '/contactus' },
+    { name: 'contact us', link: '/contactus' },
   ];
 
   closeIcon = faCircleXmark;
@@ -29,4 +33,14 @@ export class NavbarLinksComponent {
     this.navDisplay =
       this.navDisplay === 'hidden-display' ? 'show-display' : 'hidden-display';
   }
+  changeProfile(){
+    if(!this.clicked){
+      this.router.navigate([this.links[1].link]);
+    }
+    this.clicked=true;
+  }
+  unchangeProfile(){
+    this.clicked=false;
+  }
+
 }
